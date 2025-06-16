77234c7516804747aca8e446dbd712c21625b41e
Sri-2906 <sriram.s@ankercloud.com>
2025-06-16 16:31:24 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 import api from "../api";
 
 export default function Signup() {
-  const [email, setEmail] = useState("");
+  const [email, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await api.post("/signup", { email, password });
+      await api.post("/signup", { username, password });
       navigate("/login");
     } catch (err) {
       alert("Signup failed");
@@ -20,7 +20,7 @@ export default function Signup() {
   return (
     <form onSubmit={handleSignup}>
       <h2>Signup</h2>
-      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
       <button type="submit">Signup</button>
     </form>
